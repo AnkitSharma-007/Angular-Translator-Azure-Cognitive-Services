ccb057109ba60eee7b7126aed7ce82191d8e1cf4
AnkitSharma-007 <ankit.1si10cs010@gmail.com>
2020-02-17 21:53:02 +0530

    Code refactor

--- ngTranslator/ClientApp/src/app/services/translator.service.ts
+++ ngTranslator/ClientApp/src/app/services/translator.service.ts
@@ -1,29 +1,28 @@
-import { Injectable, Inject } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TranslatorService {
 
-  apiUrl = '';
+  baseURL: string;
 
-  constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    this.apiUrl = baseUrl + 'api/Translation';
+  constructor(private http: HttpClient) {
+    this.baseURL = '/api/Translation';
   }
 
   getAvailableLanguage() {
-    return this.http.get(this.apiUrl)
-      .pipe(map(response => {
+    return this.http.get(this.baseURL)
+      .pipe(response => {
         return response;
-      }));
+      });
   }
 
   getTransaltedText(textToTranslate: string, targetLanguage: string) {
-    return this.http.post(this.apiUrl + `/${textToTranslate}/${targetLanguage}`, {})
-      .pipe(map(response => {
+    return this.http.post(this.baseURL + `/${textToTranslate}/${targetLanguage}`, {})
+      .pipe(response => {
         return response;
-      }));
+      });
   }
 }
