4fe221be52054cf47f5ecc8ec88b3fd5881eb7ae
AnkitSharma-007 <ankit.1si10cs010@gmail.com>
2021-11-27 17:06:22 +0530

    updated to dotnet 6

--- ngTranslator/Controllers/TranslationController.cs
+++ ngTranslator/Controllers/TranslationController.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using ngTranslator.Models;
+using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
-using ngTranslator.Models;
+using System.Threading.Tasks;
 
 namespace ngTranslator.Controllers
 {
@@ -14,69 +13,90 @@ namespace ngTranslator.Controllers
     [Route("api/[controller]")]
     public class TranslationController : Controller
     {
+        static string subscriptionKey;
+        static string apiEndpoint;
+        static string location;
+
+        public TranslationController()
+        {
+            subscriptionKey = "Add your key";
+            apiEndpoint = "https://api.cognitive.microsofttranslator.com/";
+            location = "Add your location";
+        }
+
         [HttpPost("{textToTranslate}/{targetLanguage}")]
         public async Task<TranslationResultDTO> Post(string textToTranslate, string targetLanguage)
         {
-            TranslationResult[] translationResultList = await TranslateText(textToTranslate, targetLanguage);
+            string route = $"translate?api-version=3.0&to={targetLanguage}";
+            string requestUri = apiEndpoint + route;
 
-            TranslationResultDTO translationResult = new TranslationResultDTO
+            string result = await TranslateText(requestUri, textToTranslate);
+
+            TranslationResult[] translationResult = JsonConvert.DeserializeObject<TranslationResult[]>(result);
+
+            TranslationResultDTO translationResultDTO = new TranslationResultDTO
             {
-                DetectedLanguage = translationResultList[0].DetectedLanguage.Language,
-                TranslationOutput = translationResultList[0].Translations[0].Text
+                DetectedLanguage = translationResult[0].DetectedLanguage.Language,
+                TranslationOutput = translationResult[0].Translations[0].Text
             };
-            return translationResult;
+            return translationResultDTO;
         }
 
-        async Task<TranslationResult[]> TranslateText(string inputText, string targetLanguage)
+        async Task<string> TranslateText(string requestUri, string inputText)
         {
-            string subscriptionKey = "af19d996a3cb4a70a808567aad5bc41a";
-            string translateTextEndpoint = "https://api.cognitive.microsofttranslator.com/";
-            string route = $"/translate?api-version=3.0&to={targetLanguage}";
+            string result = string.Empty;
             object[] body = new object[] { new { Text = inputText } };
             var requestBody = JsonConvert.SerializeObject(body);
 
-            using (var client = new HttpClient())
-            using (var request = new HttpRequestMessage())
+            using var client = new HttpClient();
+            using var request = new HttpRequestMessage();
+
+            request.Method = HttpMethod.Post;
+            request.RequestUri = new Uri(requestUri);
+            request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+
+            request.Headers.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+            request.Headers.Add("Ocp-Apim-Subscription-Region", location);
+
+            HttpResponseMessage response = await client.SendAsync(request).ConfigureAwait(false);
+
+            if (response.IsSuccessStatusCode)
             {
-                request.Method = HttpMethod.Post;
-                request.RequestUri = new Uri(translateTextEndpoint + route);
-                request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
-                request.Headers.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
-
-                HttpResponseMessage responseMessage = await client.SendAsync(request).ConfigureAwait(false);
-                string result = await responseMessage.Content.ReadAsStringAsync();
-                TranslationResult[] translationResult = JsonConvert.DeserializeObject<TranslationResult[]>(result);
-
-                return translationResult;
+                result = await response.Content.ReadAsStringAsync();
             }
+
+            return result;
         }
 
         [HttpGet]
         public async Task<List<AvailableLanguageDTO>> GetAvailableLanguages()
         {
             string endpoint = "https://api.cognitive.microsofttranslator.com/languages?api-version=3.0&scope=translation";
+
             var client = new HttpClient();
-            using (var request = new HttpRequestMessage())
+            using var request = new HttpRequestMessage();
+            request.Method = HttpMethod.Get;
+            request.RequestUri = new Uri(endpoint);
+
+            var response = await client.SendAsync(request).ConfigureAwait(false);
+
+            string result = await response.Content.ReadAsStringAsync();
+            AvailableLanguage deserializedOutput = JsonConvert.DeserializeObject<AvailableLanguage>(result);
+
+            List<AvailableLanguageDTO> availableLanguage = new List<AvailableLanguageDTO>();
+
+            foreach (KeyValuePair<string, LanguageDetails> translation in deserializedOutput.Translation)
             {
-                request.Method = HttpMethod.Get;
-                request.RequestUri = new Uri(endpoint);
-                var response = await client.SendAsync(request).ConfigureAwait(false);
-                string result = await response.Content.ReadAsStringAsync();
-
-                AvailableLanguage deserializedOutput = JsonConvert.DeserializeObject<AvailableLanguage>(result);
-
-                List<AvailableLanguageDTO> availableLanguage = new List<AvailableLanguageDTO>();
-
-                foreach (KeyValuePair<string, LanguageDetails> translation in deserializedOutput.Translation)
+                AvailableLanguageDTO language = new AvailableLanguageDTO
                 {
-                    AvailableLanguageDTO language = new AvailableLanguageDTO();
-                    language.LanguageID = translation.Key;
-                    language.LanguageName = translation.Value.Name;
+                    LanguageID = translation.Key,
+                    LanguageName = translation.Value.Name
+                };
 
-                    availableLanguage.Add(language);
-                }
-                return availableLanguage;
+                availableLanguage.Add(language);
             }
+
+            return availableLanguage;
         }
     }
 }
